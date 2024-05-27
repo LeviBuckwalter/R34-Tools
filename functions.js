d399dd3f575c8643b91fc812ddfc749fc52bc82b
Levi <levibuck4@gmail.com>
2024-05-26 21:32:36 -0400

    feat: add processPosts function

--- functions.js
+++ functions.js
@@ -1,3 +1,40 @@
-export function processPosts(posts) {
-    console.log("processing posts")
+export function processPosts(posts, keepIds) {
+    //returns a new array
+
+    if (keepIds === undefined) {
+        keepIds = true
+    }
+    if (!Array.isArray(posts)) {
+        print("error: processPosts was passed something that was not an array")
+        return
+    }
+
+    let processed = []
+    for (let post of posts) {
+        let tags = post.tags.split(" ")
+        function isProblematic(tag) {
+            let problem = false
+            for (let str of problemStrs) {
+                if (tag.includes(str)) {
+                    problem = true
+                    break;
+                }
+            }
+            return problem
+        }
+        removeFromArray(tags, isProblematic)
+
+        let tagsSet = new Set()
+        for (let tag of tags) {
+            tagsSet.add(tag)
+        }
+
+        if (keepIds) {
+            processed.push({ id: post.id, tags: tagsSet })
+        } else {
+            processed.push(tagsSet)
+        }
+    }
+
+    return processed
 }
