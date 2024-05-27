cc38da4658f8539bdfee0e89e4b5e4f7e57ea60a
Levi <levibuck4@gmail.com>
2024-05-26 21:33:54 -0400

    feat: add general functions file and removeFromArray function

---
+++ general functions.js
@@ -0,0 +1,15 @@
+function removeFromArray(array, isProblem) {
+    //mutates the given array
+    //isProblem is a function which returns true or false depending on whether the item in the array should be removed
+
+    let indecesToRemove = []
+    for (let i = 0; i < array.length; i++) {
+        if (isProblem(array[i])) {
+            indecesToRemove.push(i)
+        }
+    }
+    //iterate through the array backwards when removing by index
+    for (let i = indecesToRemove.length - 1; i >= 0; i--) {
+        array.splice(indecesToRemove[i], 1)
+    }
+}
