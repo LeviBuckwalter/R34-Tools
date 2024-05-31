3388ccbfc0e949f99522c3a73ad26479c7f79c8a
Levi <levibuck4@gmail.com>
2024-05-31 14:22:45 -0400

    Instantiate FC in global variables
    Since FetchConductor is a class now instead of an object, I had to make an instance of it for use in R34 Tools.

--- global variables.js
+++ global variables.js
@@ -1,1 +1,4 @@
+import { FetchConductor } from "./classes/FetchConductor.js"
+
 export const problemStrs = ["&eacute;", "&#039;", "+", "&amp;", "&gt;", "&lt;", "&uacute;", "&ntilde;", "&iacute;"]
+export const FC = new FetchConductor(10)
