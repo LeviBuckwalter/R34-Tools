b81200484140ce119c0dc40ef9a2744894a79ff4
Levi <levibuck4@gmail.com>
2024-06-06 19:58:35 -0400

    add maxId
    right so this is to do with my concept of "anchoring." For sort of complicated reasons, I think I want to stop rule34 from adding new posts if I want my caching to work. So I'm planning to include `id:<${maxId}` in every prompt, so that no new posts past a certain point (that point being maxId) are allowed in. Every once in a while I think the user can manually reset the anchor, but it's not a big deal I feel like.

--- globals.js
+++ globals.js
@@ -2,5 +2,6 @@ import { FetchConductor } from "./classes/FetchConductor.js"
 
 export const globals = {
     problemStrs: ["&eacute;", "&#039;", "+", "&amp;", "&gt;", "&lt;", "&uacute;", "&ntilde;", "&iacute;"],
-    FC: new FetchConductor(10)
+    FC: new FetchConductor(10),
+    maxId: undefined
 }
