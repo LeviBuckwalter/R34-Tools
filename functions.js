4e341959265633513a2364473aa382389e9144b4
Levi <levibuck4@gmail.com>
2024-06-06 20:14:53 -0400

    (1/8) phase out the Search class (2) implement caching for post retrieval (3) implement hashing for cacheKeys (4) un-export processPosts (5) rename sample function to getPosts (6) redo logic inside of getPosts (7) rename fetches to promises inside of getPosts (8) add unfinished normalizePrompt function
    1: it makes more sense as a function than as a class. I was really just using it as if it was a function while it was a class.
    2: I've wanted to implement caching for post retrieval since the beginning. I don't want to be requesting the same thing from the api over and over again in short time periods. I feel like rule34 is gonna ban me.
    3: I was gonna use what is now called "cacheKey" as the cacheId, but the problem is it might have special characters that I'm not allowed to use as the name of a property in an object (which is what a chace ID is). Especially including the prompt - I mean, rule34 uses all kinds of characters in its tags. So I figured I can hash it to make it more palitable.
    4: I think I'm only gonna be using processPosts inside of this file. Especially since I'm not doing the Search class anymore (which was taking place in a different file).
    5: I like this name better? Maybe it's more consistent with "postsApi" and "postsCache".
    6: This logic is more straightforward, so I like it. But also, I don't want to ever be requesting less than 1000 posts at a time from the api, because that is pretty much un-reusable. When am I ever going to be requesting 673 posts from such-and-such search? Just request 1000, throw out what you don't need, and cache the 1000. Other people may very well need those 1000 posts later.
    7: I dunno, I think "fetch" is just a word used for too many other things. And it's a very temporary variable, and all these fetches/promises are doing is being awaited. So I think promise is a better name.
    8: It sort of says it in the documentation for the function. But yeah, I want to normalize prompts because otherwise the cache will think "feet female " is a different search from "feet female" (there's a space at the end in the first one).

--- functions.js
+++ functions.js
@@ -1,8 +1,14 @@
 import { globals } from "./globals.js"
-import { Search } from "./classes/Search.js"
+import { store, retrieve, inCacheObj } from "./Cache/functions.js"
+import { createHash } from "crypto"
 
+function hash(inputString) {
+    const h = createHash("sha256")
+    h.update(inputString)
+    return h.digest("hex")
+}
 
-export function processPosts(posts) {
+function processPosts(posts) {
     //returns a new array
 
     if (!Array.isArray(posts)) {
@@ -45,21 +51,54 @@ export function processPosts(posts) {
     return processed
 }
 
-export async function sample(prompt, amtPosts) {
-    /* returns an array of processed posts */
-    const batches = Math.ceil(amtPosts / 1000)
-    const finalBatchAmt = amtPosts - ((batches - 1) * 1000)
+async function postsApi(prompt, pid, limit, json) {
+    prompt = (prompt === undefined) ? "" : prompt
+    pid = (pid === undefined) ? 0 : pid
+    limit = (limit === undefined) ? 1000 : limit
+    json = (json === undefined) ? true : json
+    const url = `https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&tags=${prompt}&pid=${pid}&limit=${limit}&json=${(json === true) ? 1 : 0}`
 
-    let fetches = []
-    for (let pid = 0; pid < batches - 1; pid++) {
-        fetches.push(new Search(prompt, pid).fetch())
+    async function myEgg() {
+        let resp = await fetch(url)
+        return processPosts(await resp.json())
     }
-    fetches.push(new Search(prompt, batches - 1, finalBatchAmt).fetch())
+    const ret = await globals.FC.ticket(myEgg)
+    return ret
+}
+
+async function postsCache(prompt, pid, limit, json) {
+    const cacheKey = "postsApi" + prompt + pid + limit + json
+    const cacheId = hash(cacheKey)
+
+    if (inCacheObj(cacheId)) {
+        return retrieve(cacheId)
+    } else {
+        let posts = await postsApi(prompt, pid, limit, json)
+        store(posts, cacheId)
+        return posts
+    }
+}
+
+export async function getPosts(prompt, amtPosts) {
+    /* returns an array of processed posts */
+    const pages = Math.ceil(amtPosts / 1000)
+
+    let promises = []
+    for (let pid = 0; pid < pages; pid++) {
+        promises.push(postsCache(prompt, pid))
+    }
 
     let posts = []
-    for (let fetch of fetches) {
-        posts.push(...processPosts(await fetch))
+    for (let promise of promises) {
+        posts.push(...await promise)
     }
 
-    return posts
+    return posts.slice(0, amtPosts)
+}
+
+function normalizePrompt(prompt) {
+    /*
+    I will want this function to remove double spaces and spaces from the end or beginning of the prompt
+    */
+    return prompt
 }
