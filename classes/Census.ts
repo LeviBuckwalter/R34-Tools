70284884619eed4b2c5cfb90694606ba08d5eb5f
Levi <levibuck4@gmail.com>
2024-07-01 21:01:47 -0400

    add seed functionality to census object

--- classes/Census.ts
+++ classes/Census.ts
@@ -4,18 +4,23 @@ export class Census {
     counts: {[tag: string]: number}
     size: number
 
-    constructor(posts: Post[]) {
-        this.counts = {}
-        this.size = 0
-        for (const post of posts) {
-            this.size++
-            for (const tag of post.tags.values()) {
-                if (tag in this.counts) {
-                    this.counts[tag]++
-                } else {
-                    this.counts[tag] = 1
+    constructor(given: Post[] | {counts: {[tag: string]: number}, size: number}) {
+        if (Array.isArray(given)) {
+            this.counts = {}
+            this.size = 0
+            for (const post of given) {
+                this.size++
+                for (const tag of post.tags.values()) {
+                    if (tag in this.counts) {
+                        this.counts[tag]++
+                    } else {
+                        this.counts[tag] = 1
+                    }
                 }
             }
+        } else {
+            this.counts = given.counts
+            this.size = given.size
         }
     }
 
@@ -25,5 +30,26 @@ export class Census {
         } else {
             return 0
         }
+    }
+
+    toArray(amtTags?: number): {tag: string, count: number}[] {
+        const tags: string[] = Object.keys(this.counts)
+        const ret: {tag: string, count: number}[] = []
+        for (const tag of tags) {
+            ret.push({tag: tag, count: this.counts[tag]})
+        }
+        ret.sort(function(a, b) {
+            return b.count - a.count
+        })
+        
+        if (amtTags) {
+            return ret.slice(0, amtTags)
+        } else {
+            return ret
+        }
+    }
+
+    toSeed(): {counts: {[tag: string]: number}, size: number} {
+        return {counts: this.counts, size: this.size}
     }
 }
