3a11acbe5e8e86848aaed6cafb27be8cbb0b15c7
Levi <levibuck4@gmail.com>
2024-07-01 21:00:41 -0400

    remove and put into caches file

--- main.ts
+++ main.ts
@@ -1,8 +1,5 @@
 import { FetchConductor } from "./classes/FetchConductor.ts"
-import { Cache } from "cache/classes/Cache.ts"
-import { Post } from "./classes/Post.ts";
+
 
 export const FC: FetchConductor = new FetchConductor(10)
 //the count cache houses individual percentages of tags ({percent: 0.2314, amtPosts: 1000}) indexed by prompt+tag
-export const PercentCache: Cache<{percent: number, amtPosts: number, allPostsChecked: boolean}> = new Cache("PercentCache", 5)
-export const GeneralCache: Cache<any> = new Cache("GeneralCache", Infinity)
