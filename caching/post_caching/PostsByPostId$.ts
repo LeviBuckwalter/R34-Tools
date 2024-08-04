677f0993c5db0f6547e53f7da01ee1420a408ce5
Levi <levibuck4@gmail.com>
2024-08-03 20:49:39 -0400

    reorganize files

--- caching/post_caching/PostsByPostId$.ts
+++
@@ -1,8 +0,0 @@
-import { Post } from "../../classes/Post.ts";
-import { Cache } from "../../Cache/classes/Cache.ts";
-
-/*
-stores posts, indexed by postIds.
-*/
-
-export const PostsByPostId$: Cache<Post> = new Cache("PostsByPostId$", 50000, [Post], true)
--- caching/prompt_count_cache/PromptCount$.ts
+++
@@ -1,3 +0,0 @@
-import { Cache } from "../../Cache/classes/Cache.ts"
-
-export const PromptCount$: Cache<number> = new Cache("PromptCount$", 50000)
